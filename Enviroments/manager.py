ea587f17c8d7a14c5d0f58e9d86c59f2218a3d17
Daniel Afrimi <daniel.afrimi@mail.huji.ac.il>
2020-07-14 17:53:00 +0300

    Changes in the API

--- Enviroments/manager.py
+++ Enviroments/manager.py
@@ -2,48 +2,44 @@ import networkx as nx
 from typing import List
 from routing.route_selection import get_route
 
-class Manager:
 
+class Manager:
     def __init__(self, graph: nx.Graph):
         self.graph: nx.Graph = graph
-        self.counter = 0
-
 
     def transfer(self, amount: int, src, dst):
         path: List[nx.Graph.edges] = get_route(self.graph, src, dst)
-
 
     def get_state(self):
         return self.graph
 
     def step(self, action) -> nx.Graph:
         if action[0] == "add_edge":
-            return self.add_edge(*action[1])
+            self.add_edge(*action[1])
+        elif action[0] == "NOOP":
+            pass
         else:
             raise ValueError(f"{action} not supported ")
 
-
-    def route(self, routing_type: str) -> List[nx.Graph.edges]:
-        pass
+        return self.graph
 
     def is_transfer_valid(self, amount: int):
         pass
 
     def create_agent_node(self):
-        self.graph.add_node(len(self.graph.nodes) + 1)
+        pub_key = len(self.graph.nodes) + 1
+        self.graph.add_node(pub_key)
+        return pub_key
 
-    def add_edge(self, pub_key_node1, pub_key_node2, node1_balance, node2_balance) -> nx.Graph:
+    def add_edge(self, pub_key_node1, pub_key_node2, node1_balance, node2_balance):
         capacity = node1_balance + node2_balance
         self.graph.add_edge(pub_key_node1, pub_key_node2, capcity=capacity,
-                      node1_balance=node1_balance, node2_balance=node2_balance)
-        return self.graph
+                            node1_balance=node1_balance, node2_balance=node2_balance)
 
-    def get_node_balance(self, node1):
-        connected_edges = self.graph.edges([node1])
-
+    def get_node_balance(self, node1_pub_key):
+        connected_edges = self.graph.edges(nbunch=node1_pub_key, data=True)
 
 
-
-def initialize_graph(type: str) -> nx.Graph:
-    pass
-
+#
+# def initialize_graph(type: str) -> nx.Graph:
+#     pass
