79ad2fcf0055f3893307ca20baef6f8f259bd7b3
Alon Netser <alonnetser3@gmail.com>
2020-08-08 17:21:50 +0300

    Added channel's openning cost (from the actual lightning network)

--- consts.py
+++ consts.py
@@ -14,5 +14,13 @@ AGENT_MAX_FUNDS = 10 ** 10
 # TODO: Split agent score to locked money and channel opening cost
 LN_DEFAULT_CHANNEL_COST = 0
 
+# The channel creation cost (which is the cost payed for the bitcoin miners
+# to include the channel's creation transaction in their block).
+# This value changes constantly (due to the dynamics of the bitcoin transactions' fees
+# that change according to the load on the blockchain).
+# This approximate value was calculated using buybitcoinworldwide.com to get the cost
+# of a transaction (in usd), then converting us dollars to satoshis (in 8.8.2020).
+CHANNEL_CREATION_COST = 40000
+
 # TODO: Find the actual default values in real life
 LND_DEFAULT_POLICY = {"time_lock_delta": 144, "fee_base_msat": 1000, "fee_rate_milli_msat": 0.001}
